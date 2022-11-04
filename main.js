d7762687ba92962a219542eaccb6329bb9429a83
Jan Varmuza <jan.varmuza@gmail.com>
2022-11-04 20:44:28 +0100

    add main.js and edit index.html
    simple UI for out calculator (text input) & basic logic for getting the input value, setting a regex for operators

---
+++ main.js
@@ -0,0 +1,9 @@
+const calculate = (e) => {
+  const inputValue = e.target.value;
+  const regex = /[-\/\*\+]/;
+  const numA;
+  const numB;
+  const operator;
+};
+
+document.getElementById("inputValue").addEventListener("change", calculate);
